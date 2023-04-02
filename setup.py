0feadcf44f9aecf1937b8b687cccde2e9e152e37
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-04-02 17:48:02 +0200

    bump minor 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name="tree_utils",
     packages=setuptools.find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     install_requires=["jax", "jaxlib", "dm-tree"],
 )
